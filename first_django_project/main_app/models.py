111ae64e84a7b83cc19dc6a649e0a64fb6543567
shazedul-jewel <jewel8102631@gmail.com>
2025-01-04 23:24:11 +0600

    Database

--- first_django_project/main_app/models.py
+++ first_django_project/main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to='profileimage')
